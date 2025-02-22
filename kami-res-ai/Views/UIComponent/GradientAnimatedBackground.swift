44c8dedf1ed73d95d9643248aeb025f9cd0cb3d5
Kojiro Tsugaru <kojirotsugaru@gmail.com>
2025-02-22 17:38:17 +0900

    added Package.swift

---
+++ kami-res-ai/Views/UIComponent/GradientAnimatedBackground.swift
@@ -0,0 +1,18 @@
+//
+//  GradientAnimatedBackground.swift
+//  kami-res-ai
+//
+//  Created by KJ on 2/22/25.
+//
+
+import SwiftUI
+
+struct GradientAnimatedBackground: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    GradientAnimatedBackground()
+}
