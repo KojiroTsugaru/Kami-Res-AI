5076832d9357ba64be783ca1e370f8e81c7cbe0e
Kojiro Tsugaru <93096898+KojiroTsugaru@users.noreply.github.com>
2025-03-30 16:38:37 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,8 +4,7 @@ import PackageDescription
 let package = Package(
     name: "kami-res-ai",
     platforms: [
-        .iOS(.v15),
-        .macOS(.v10_12)
+        .iOS(.v12)
     ],
     products: [
         .executable(name: "kami-res-ai", targets: ["kami-res-ai"])
