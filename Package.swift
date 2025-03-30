33aef9c77fd59acba2f3197d01b2b37abd33333b
Kojiro Tsugaru <93096898+KojiroTsugaru@users.noreply.github.com>
2025-03-30 15:56:20 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,30 +15,17 @@ let package = Package(
         .package(
             url: "https://github.com/realm/realm-swift.git",
             .branch("master")
-        ),
-        
-        // If you have a separate "RealmDatabase" package at version 14.14.0
-        .package(
-            url: "https://github.com/YourOrg/RealmDatabase.git",
-            exact: "14.14.0"
-        ),
-        
-        // Superscript at version 0.1.16
-        .package(
-            url: "https://github.com/YourOrg/Superscript.git",
-            exact: "0.1.16"
-        ),
-        
+        ),    
         // SuperwallKit at version 3.12.2
         // (Update URL if different; this is the current known SuperwallKit repo)
         .package(
             url: "https://github.com/superwall-me/superwall-ios-sdk.git",
-            exact: "3.12.2"
+            from: "3.0.0"
         ),
         
         // SwiftfulLoadingIndicators at version 0.0.4
         .package(
-            url: "https://github.com/YourOrg/SwiftfulLoadingIndicators.git",
+            url: "https://github.com/SwiftfulThinking/SwiftfulLoadingIndicators",
             exact: "0.0.4"
         )
     ],
@@ -48,8 +35,6 @@ let package = Package(
             dependencies: [
                 // Update product names to match each package’s product definition:
                 .product(name: "RealmSwift", package: "realm-swift"),
-                .product(name: "RealmDatabase", package: "RealmDatabase"),
-                .product(name: "Superscript", package: "Superscript"),
                 .product(name: "SuperwallKit", package: "superwall-ios-sdk"),
                 .product(name: "SwiftfulLoadingIndicators", package: "SwiftfulLoadingIndicators")
             ],
